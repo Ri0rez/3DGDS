b2aca1547505886c2139148952b225caa213283f
Ri0rez <bgpome@gmail.com>
2024-07-26 11:45:19 +0100

    commit initial

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+/*
+var code = parseInt(prompt('Entrez le code secret'));
+
+
+if (code ==12459) {
+    alert('votre mot de passe est correct');
+    alert('Bienvenu sur votre site');
+}
+
+else{
+    alert('Le mot de passe est éronné')
+}
+*/
+
+// var age = parseInt(prompt('Quel âge avez vous ?'))
+
+// if (1<=age && age<=6){
+//     alert('Vous êtes un jeune enfant.')
+// }
+// else if (7<=age && age<=11){
+//     alert('Vous êtes un enfant qui a atteint l\'âge de raison.')
+// }
+// else if (12<=age && age<=17){
+//     alert('Vous êtes un adolescent.')
+// }
+// else if (18<=age && age<=120){
+//     alert('Vous êtes un adulte.')
+// }
+// else{
+//     alert('Soit vous n\'êtes pas humain, soit vous êtes une personne avec une très longue vie.')
+// }
+
+
+
